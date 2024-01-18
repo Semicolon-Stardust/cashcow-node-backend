d00e651e276735e14d3130358098e7bbc624f529
muteenk <kundangarmuteen@gmail.com>
2024-01-18 21:58:50 +0530

    Error Handling

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const app = require("./app");
 const dotenv = require("dotenv");
 dotenv.config({path: __dirname+"/config/config.env"});
+const connectDatabase = require("./config/database");
+
+// Handle Uncaught Exceptions
+process.on("uncaughtException", err => {
+    console.log(`Error: ${err.message}`);
+    console.log("Shutting down the server due to Uncaught Exception");
+    process.exit(1);
+})
 
 
 const port = process.env.PORT || 8000;
@@ -12,4 +20,14 @@ connectDatabase();
 
 const server = app.listen(port, () => {
     console.log(`server started at http://127.0.0.1:${port}`);
+})
+
+
+// Unhandled Promise Rejection
+process.on("unhandledRejection", err => {
+    console.log(`Error: ${err.message}`);
+    console.log("Shutting down the server due to Unhandled Promise Rejection");
+    server.close(() => {
+        process.exit(1);
+    });
 })
