c8614f6b9f6c5dd23c068e1280f8596aed787b84
muteenk <kundangarmuteen@gmail.com>
2024-01-18 21:56:27 +0530

    Updated App

--- app.js
+++ app.js
@@ -8,9 +8,19 @@ app.use(cookieParser());
 
 // Route Imports
 const user = require("./routes/userRoute");
+const transaction = require("./routes/transactionRoute");
+
+// Middleware Imports
+const errorMiddleware = require("./middleware/error");
+
 
 // Mount Routes
 app.use("/api/v1", user);
+app.use("/api/v1/transaction", transaction);
+
+
+// Middleware for Errors
+app.use(errorMiddleware);
 
 
 module.exports = app;
