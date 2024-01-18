63b4eb3cbf915fd8660591c3a4f2d6dcb457f5aa
muteenk <kundangarmuteen@gmail.com>
2024-01-18 20:03:37 +0530

    Updated routes

--- app.js
+++ app.js
@@ -6,5 +6,11 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+// Route Imports
+const user = require("./routes/userRoute");
+
+// Mount Routes
+app.use("/api/v1", user);
+
 
 module.exports = app;
--- server.js
+++ server.js
@@ -6,6 +6,10 @@ dotenv.config({path: __dirname+"/config/config.env"});
 const port = process.env.PORT || 8000;
 
 
+// Connect to database
+connectDatabase();
+
+
 const server = app.listen(port, () => {
     console.log(`server started at http://127.0.0.1:${port}`);
 })
