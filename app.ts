755c8191226e06b3359ef6c18581b4726f452c46
muteenk <kundangarmuteen@gmail.com>
2024-06-13 12:55:55 +0530

    base routing done

--- app.ts
+++ app.ts
@@ -1,9 +1,34 @@
 import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
 
+import errorMiddleware from './middleware/error.js';
+import baseRouter from './routes/baseRoutes.js';
+
+
+// Init
 const app = express();
+const corsOptions = {
+    origin: "*",
+    credentials: true,
+}
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-})
+
+
+// Middlewares
+/*-- General --*/
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended: false}))
+app.use(cors(corsOptions));
+/*-- Personal --*/
+app.use(errorMiddleware);
+
+
+
+// Routes
+app.use('/api/v1', baseRouter);
+
+
 
 export default app;
