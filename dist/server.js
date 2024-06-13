0fb5f9487a02ff15d87ddbbc2233a047d58b7b7f
muteenk <kundangarmuteen@gmail.com>
2024-06-13 12:08:48 +0530

    Database Connection

--- dist/server.js
+++ dist/server.js
@@ -1,7 +1,9 @@
 import dotenv from 'dotenv';
 dotenv.config({ path: './config/config.env' });
+import connectDatabase from './config/database.js';
 import app from './app.js';
 const port = process.env.PORT || 5000;
+connectDatabase();
 const server = app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
--- server.ts
+++ server.ts
@@ -1,11 +1,12 @@
 import dotenv from 'dotenv';
 dotenv.config({path: './config/config.env'});
 
-
+import connectDatabase from './config/database.js';
 import app from './app.js';
 
-
 const port = process.env.PORT || 5000;
+
+connectDatabase();
 
 
 const server = app.listen(port, () => {
